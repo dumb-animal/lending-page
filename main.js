fd3fec403f96a0ecc8bdaf5e16c49bf6b8e659b0
dumb-animal <dumb_animal@icloud.com>
2020-12-20 03:34:39 +0300

    update tabs

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-const tab1 = document.getElementById('tab_01')
-const tab2 = document.getElementById('tab_02')
-const tab3 = document.getElementById('tab_03')
-
-const link1 = document.getElementById('tab-link_01')
-const link2 = document.getElementById('tab-link_02')
-const link3 = document.getElementById('tab-link_03')
-
-const tabs = [tab1, tab2, tab3]
-const links = [link1, link2, link3]
+const links = [...document.getElementsByClassName('tabs__item')]
+const tabs = [...document.getElementsByClassName('tabs__block')]
 
 const showTab = ({ target }) => {
   const id = parseInt(target.id[target.id.length - 1]) - 1
+
+  if (links[id].classList.contains('disabled')) return
+
   links.forEach((link) => link.classList.remove('active'))
-  links[id].classList.add('active')
   tabs.forEach((tab) => (tab.style.display = 'none'))
+
+  links[id].classList.add('active')
   tabs[id].style.display = 'block'
 }
 
-link1.addEventListener('click', showTab)
-link2.addEventListener('click', showTab)
-link3.addEventListener('click', showTab)
+links.forEach((link) => link.addEventListener('click', showTab))
